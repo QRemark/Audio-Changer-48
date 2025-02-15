5c4d129b34d4102827e6839718fce7119b57f1e1
QRemark <S099641108@yandex.ru>
2025-02-15 16:13:23 +0300

    переключает но пока мьютит

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
-using System.Linq;
 using System.Threading;
-using NAudio.CoreAudioApi;
 
 class Program
 {
-    // Импортируем функцию для перехвата клавиш
     [DllImport("user32.dll")]
     public static extern short GetAsyncKeyState(int vKey);
 
@@ -21,12 +19,12 @@ class Program
 
             while (true)
             {
-                Console.WriteLine("Ожидание клавиши..."); // Проверка работы цикла
-                if ((GetAsyncKeyState(0xAD) & 0x8000) != 0) // 0xAD - клавиша Mute
+                Console.WriteLine("Ожидание клавиши...");
+                if ((GetAsyncKeyState(0xAD) & 0x8000) != 0)
                 {
                     Console.WriteLine("Клавиша Mute нажата, переключение...");
                     ToggleAudioDevice();
-                    Thread.Sleep(500); // Задержка, чтобы избежать повторного срабатывания
+                    Thread.Sleep(500);
                 }
                 Thread.Sleep(50);
             }
@@ -34,7 +32,7 @@ class Program
         catch (Exception ex)
         {
             Console.WriteLine($"Ошибка: {ex.Message}\n{ex.StackTrace}");
-            Console.ReadLine(); // Оставить консоль открытой для просмотра ошибки
+            Console.ReadLine();
         }
     }
 
@@ -42,60 +40,90 @@ class Program
     {
         try
         {
-            var enumerator = new MMDeviceEnumerator();
-            var devices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active).ToList();
-            var currentDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            string currentDeviceId = GetCurrentAudioDeviceId();
+            string targetDeviceId = currentDeviceId == GetDeviceId(device1) ? GetDeviceId(device2) : GetDeviceId(device1);
 
-            Console.WriteLine("Текущий аудиовыход: " + currentDevice.FriendlyName);
-            Console.WriteLine("Доступные аудиоустройства:");
-            foreach (var device in devices)
-            {
-                Console.WriteLine(" - " + device.FriendlyName);
-            }
+            Console.WriteLine($"Переключение на устройство с ID: {targetDeviceId}");
 
-            var targetDevice = devices.FirstOrDefault(d => d.FriendlyName.Contains(currentDevice.FriendlyName == device1 ? device2 : device1));
-
-            if (targetDevice != null)
-            {
-                var policyConfig = new PolicyConfigClient();
-                policyConfig.SetDefaultEndpoint(targetDevice.ID);
-                Console.WriteLine($"Переключено на: {targetDevice.FriendlyName}");
-            }
-            else
-            {
-                Console.WriteLine("Устройство не найдено! Убедитесь, что название указано верно.");
-            }
+            SetAudioDevice(targetDeviceId);
+            UnmuteAudio(); // Снимаем Mute после переключения
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Ошибка при переключении устройства: {ex.Message}\n{ex.StackTrace}");
         }
     }
-}
 
-// Класс для смены устройства по умолчанию
-[ComImport]
-[Guid("568b9108-44bf-40b4-9006-86afe5b5a620")]
-[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-interface IPolicyConfig
-{
-    [PreserveSig]
-    int SetDefaultEndpoint([MarshalAs(UnmanagedType.LPWStr)] string wszDeviceId, int eRole);
-}
-
-class PolicyConfigClient
-{
-    public void SetDefaultEndpoint(string deviceId)
+    static string GetCurrentAudioDeviceId()
     {
-        try
+        ProcessStartInfo psi = new ProcessStartInfo
         {
-            Type policyConfigType = Type.GetTypeFromCLSID(new Guid("568b9108-44bf-40b4-9006-86afe5b5a620"));
-            IPolicyConfig policyConfig = (IPolicyConfig)Activator.CreateInstance(policyConfigType);
-            policyConfig.SetDefaultEndpoint(deviceId, 0);
-        }
-        catch (Exception ex)
+            FileName = "powershell",
+            Arguments = "-Command \"(Get-AudioDevice -Playback).ID\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using (Process process = Process.Start(psi))
         {
-            Console.WriteLine($"Ошибка при установке устройства по умолчанию: {ex.Message}");
+            process.WaitForExit();
+            return process.StandardOutput.ReadLine().Trim();
         }
     }
+
+    static string GetDeviceId(string deviceName)
+    {
+        ProcessStartInfo psi = new ProcessStartInfo
+        {
+            FileName = "powershell",
+            Arguments = $"-Command \"(Get-AudioDevice -List | Where-Object {{$_.Name -eq '{deviceName}'}}).ID\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using (Process process = Process.Start(psi))
+        {
+            process.WaitForExit();
+            return process.StandardOutput.ReadLine()?.Trim() ?? "";
+        }
+    }
+
+    static void SetAudioDevice(string deviceId)
+    {
+        ProcessStartInfo psi = new ProcessStartInfo
+        {
+            FileName = "powershell",
+            Arguments = $"-Command \"Set-AudioDevice -ID '{deviceId}'\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using (Process process = Process.Start(psi))
+        {
+            process.WaitForExit();
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+        }
+    }
+
+    static void UnmuteAudio()
+    {
+        ProcessStartInfo psi = new ProcessStartInfo
+        {
+            FileName = "powershell",
+            Arguments = "-Command \"(Get-AudioDevice -Playback) | Set-Volume -Volume 50; (Get-AudioDevice -Playback) | Set-Volume -Mute $false\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using (Process process = Process.Start(psi))
+        {
+            process.WaitForExit();
+            Console.WriteLine("Громкость установлена на 50%, звук включен после переключения.");
+        }
+    }
+
 }
